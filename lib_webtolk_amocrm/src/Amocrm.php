f00ee9caeec525dc28b7518ee6e9f35f1cce2b8c
Sergey Tolkachyov <musstudent@mail.ru>
2023-10-17 11:00:15 +0400

    Обновляем копирайты v.1.2.0

--- lib_webtolk_amocrm/src/Amocrm.php
+++ lib_webtolk_amocrm/src/Amocrm.php
@@ -2,9 +2,9 @@
 
 /**
  * @package       WT Amocrm Library
- * @version       __DEPLOY_VERSION__
+ * @version       1.2.0
  * @Author        Sergey Tolkachyov, https://web-tolk.ru
- * @сopyright (c) 2022 - September 2023 Sergey Tolkachyov. All rights reserved.
+ * @сopyright (c) 2022 - October 2023 Sergey Tolkachyov. All rights reserved.
  * @license       GNU/GPL3 http://www.gnu.org/licenses/gpl-3.0.html
  * @since         1.0.0
  */
