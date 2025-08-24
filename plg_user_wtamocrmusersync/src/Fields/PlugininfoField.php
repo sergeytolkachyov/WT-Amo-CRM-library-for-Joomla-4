094711bb1ea9d5bcf660ff3b48f2b4d6e743fef7
gug2 <andrei.smirnikov@yandex.ru>
2025-08-24 20:21:05 +0400

    Возвращение предыдущей верстки для поля информации о плагине

--- plg_user_wtamocrmusersync/src/Fields/PlugininfoField.php
+++ plg_user_wtamocrmusersync/src/Fields/PlugininfoField.php
@@ -33,7 +33,41 @@ class PlugininfoField extends NoteField
 	 */
 	protected function getInput()
 	{
-		return '';
+        $data = $this->form->getData();
+        $element = $data->get('element');
+        $folder = $data->get('folder');
+        $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+        $wa->addInlineStyle("
+			.plugin-info-img-svg:hover * {
+				cursor:pointer;
+			}
+		");
+
+        $wt_plugin_info = simplexml_load_file(JPATH_SITE . "/plugins/" . $folder . "/" . $element . "/" . $element . ".xml");
+
+        return '<div class="d-flex shadow p-4">
+			<div class="flex-shrink-0">
+				<a href="https://web-tolk.ru" target="_blank">
+					<svg class="plugin-info-img-svg" width="200" height="50" xmlns="http://www.w3.org/2000/svg">
+						<g>
+							<title>Go to https://web-tolk.ru</title>
+							<text font-weight="bold" xml:space="preserve" text-anchor="start"
+							      font-family="Helvetica, Arial, sans-serif" font-size="32" id="svg_3" y="36.085949"
+							      x="8.152073" stroke-opacity="null" stroke-width="0" stroke="#000"
+							      fill="#0fa2e6">Web</text>
+							<text font-weight="bold" xml:space="preserve" text-anchor="start"
+							      font-family="Helvetica, Arial, sans-serif" font-size="32" id="svg_4" y="36.081862"
+							      x="74.239105" stroke-opacity="null" stroke-width="0" stroke="#000"
+							      fill="#384148">Tolk</text>
+						</g>
+					</svg>
+				</a>
+			</div>
+			<div class="flex-grow-1 ms-3">
+				<span class="badge bg-success text-white">v.' . $wt_plugin_info->version . '</span>
+				' . Text::_("PLG_".strtoupper($element)."_DESC") . '
+			</div>
+		</div>';
 	}
 
 	/**
@@ -43,47 +77,7 @@ class PlugininfoField extends NoteField
 	 */
 	protected function getLabel()
 	{
-        $data = $this->form->getData();
-        $element = $data->get('element');
-        $folder = $data->get('folder');
-        $wt_plugin_info = simplexml_load_file(JPATH_SITE."/plugins/".$folder."/".$element."/".$element.".xml");
-
-        /* @var $doc \Joomla\CMS\Document\Document */
-        $doc = Factory::getApplication()->getDocument();
-        $doc->getWebAssetManager()->addInlineStyle('
-            #web_tolk_link {
-			text-align: center;
-			}
-			#web_tolk_link::before{
-				content: "";
-			}
-        ');
-
-        return '</div>
-		<div class="card container shadow-sm w-100 p-0">
-			<div class="wt-b24-plugin-info row">
-				<div class="col-2 d-flex justify-content-center align-items-center">
-					<a href="https://web-tolk.ru" target="_blank" id="web_tolk_link" title="Go to https://web-tolk.ru">
-                        <svg width="200" height="50" viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg">
-                             <g>
-                              <title>Go to https://web-tolk.ru</title>
-                              <text font-weight="bold" xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="32" id="svg_3" y="36.085949" x="8.152073" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#0fa2e6">Web</text>
-                              <text font-weight="bold" xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="32" id="svg_4" y="36.081862" x="74.239105" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#384148">Tolk</text>
-                             </g>
-                        </svg>
-				    </a>
-				</div>
-				<div class="col-10">
-					<div class="card-header bg-white p-1">
-						<span class="badge bg-success">v.' . $wt_plugin_info->version . '</span>
-					</div>
-					<div class="card-body">
-						' . Text::_("PLG_".strtoupper($element)."_DESC") . '
-					</div>
-				</div>
-			</div>
-		</div><div>
-		';
+        return '';
 	}
 
 	/**
