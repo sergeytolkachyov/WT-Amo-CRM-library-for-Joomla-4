29e2228b279f6941e50426638de8becfff29d9ed
Sergey Tolkachyov <musstudent@mail.ru>
2025-09-29 21:34:23 +0400

    Правки после тестов на обновление с предыдущих версий.

--- plg_system_wt_amocrm/sql/updates/mysql/1.3.0.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `#__lib_wt_amocrm_users_sync` ADD `is_temporary_user` int(1) NOT NULL DEFAULT '0' COMMENT 'Flag that this is a temporary joomla user data. Need to complete registration';
